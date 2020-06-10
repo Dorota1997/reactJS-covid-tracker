c12afaf0e666df1a17a1c262dd4622393ca7fc68
Dorota Gil <gildota@wp.pl>
2020-06-10 23:12:06 +0200

    Add api fetch data

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async () => {
+    try {
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+
+        const modifiedData = { confirmed, recovered, deaths, lastUpdate }
+
+        return modifiedData;
+    } catch(error) {
+
+    }
+}
