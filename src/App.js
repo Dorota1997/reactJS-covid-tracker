4c1273d665ee81811180f585bc2dddf23ad30d40
Dorota Gil <gildota@wp.pl>
2020-06-12 23:59:35 +0200

    Create handleCountryChange method

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import { Cards, Chart, CountryPicker } from './components';
 import { fetchData } from './api';
+import covidImage from './images/covid.png';
 
 class App extends React.Component {
   state = {
     data: {},
+    country: '',
   }
 
   async componentDidMount() {
@@ -13,14 +15,20 @@ class App extends React.Component {
     this.setState({ data: fetchedData });
   }
 
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  }
+
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
 
     return (
       <div className="container">
+        <img className="image" src={covidImage} alt="COVID-19" />
         <Cards data={data} />
-        <Chart />
-        <CountryPicker />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
     );
   }
