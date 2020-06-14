98bbfed42bb4561c956b216fb4c11e23d13ce109
Dorota Gil <gildota@wp.pl>
2020-06-14 19:32:30 +0200

    Change fetch methods

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,18 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+    let changeableUrl = url;
+
+    if (country) {
+        changeableUrl = `${url}/countries/${country}`;
+    }
+
     try {
-        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
 
         const modifiedData = { confirmed, recovered, deaths, lastUpdate }
-
+        console.log(modifiedData);
         return modifiedData;
     } catch(error) {
         console.log(error);
@@ -21,11 +27,23 @@ export const fetchDailyData = async () => {
         const modifiedData = data.map((dailyData) => ({
             confirmed: dailyData.confirmed.total,
             deaths: dailyData.deaths.total,
+            recovered: dailyData.recovered.total,
             date: dailyData.reportDate
         }));
 
+        console.log(modifiedData);
         return modifiedData;
     } catch (error) {
-        
+        console.log(error);
+    }
+}
+
+export const fetchCountries = async () => {
+    try {
+        const { data : { countries }} = await axios.get(`${url}/countries`);
+
+        return countries.map((country) => country.name);
+    } catch (error) {
+        console.log(error);
     }
 }
